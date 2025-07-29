53e5a708d2de71ad3917f6c00e568fe4a2f04899
FearsRedemption <darkworldmage@gmail.com>
2025-07-29 15:08:43 -0500

    part 1 retry

--- src/data/allJobsData.ts
+++ src/data/allJobsData.ts
@@ -1,29 +1,5 @@
 ﻿// src/data/allJobsData.ts
-import Paladin from '../assets/images/jobs/Paladin.png';
-import Warrior from '../assets/images/jobs/Warrior.png';
-import DarkKnight from '../assets/images/jobs/Dark Knight.png';
-import Gunbreaker from '../assets/images/jobs/Gunbreaker.png';
 
-import WhiteMage from '../assets/images/jobs/White Mage.png';
-import Scholar from '../assets/images/jobs/Scholar.png';
-import Astrologian from '../assets/images/jobs/Astrologian.png';
-import Sage from '../assets/images/jobs/Sage.png';
-
-import Monk from '../assets/images/jobs/Monk.png';
-import Dragoon from '../assets/images/jobs/Dragoon.png';
-import Ninja from '../assets/images/jobs/Ninja.png';
-import Samurai from '../assets/images/jobs/Samurai.png';
-import Reaper from '../assets/images/jobs/Reaper.png';
-import Viper from '../assets/images/jobs/Viper.png';
-
-import Bard from '../assets/images/jobs/Bard.png';
-import Machinist from '../assets/images/jobs/Machinist.png';
-import Dancer from '../assets/images/jobs/Dancer.png';
-
-import BlackMage from '../assets/images/jobs/Black Mage.png';
-import Summoner from '../assets/images/jobs/Summoner.png';
-import RedMage from '../assets/images/jobs/Red Mage.png';
-import BlueMage from '../assets/images/jobs/Blue Mage.png';
 
 export interface JobCardData {
     jobName: string;
@@ -38,14 +14,6 @@ export const allJobsData: JobCardData[] = [
     // Tanks
     {
         jobName: "Paladin",
-        icon: Paladin,
-        jobRole: "Tank",
-        unlockedIn: "A Realm Reborn",
-        difficulty: 2,
-        link: '#'
-    },
-    {
-        jobName: "Fake Tank",
         icon: Paladin,
         jobRole: "Tank",
         unlockedIn: "A Realm Reborn",
